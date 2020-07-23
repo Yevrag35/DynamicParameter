f88d8314dc6bbb90679675700d85d4834e0de0c7
Mike Garvey <mike.garvey@yevrag35.com>
2020-07-23 17:24:11 -0400

    TestClass

--- MG.Dynamic-DotNet/TestEnv/TestClass.cs
+++ MG.Dynamic-DotNet/TestEnv/TestClass.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MG.Dynamic.Parameter;
+using MG.Dynamic.Library;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Management.Automation;
 
 namespace MG.Dynamic.Testing
@@ -14,20 +17,47 @@ namespace MG.Dynamic.Testing
         [Parameter(Mandatory = false)]
         public string TestString { get; set; }
 
-        private IDynamicDefiner _dyn;
+        public ComplexDynamicParameter<Employee, string> Employee { get; private set; }
+
+
         private RuntimeDefinedParameterDictionary dict;
 
         public object GetDynamicParameters()
         {
+            dict = new RuntimeDefinedParameterDictionary();
+            var emp = new Employee
+            {
+                Id = 72625635,
+                Name = "Jim Jones"
+            };
+            this.Employee = new ComplexDynamicParameter<Employee, string>("Employee")
+            {
+                Mandatory = true,
+                Position = 0,
+                ValueFromPipeline = true
+            };
+            this.Employee.SetDynamicItems(new Employee[1] { emp }, true, x => x.Name);
 
+            dict.Add(this.Employee.Name, this.Employee.AsRuntimeDefinedParameter());
+            return dict;
         }
 
-        protected override void BeginProcessing() => base.BeginProcessing();
+        protected override void BeginProcessing() { }
 
         protected override void ProcessRecord()
         {
-            string[] chosen = dict[pName].Value as string[];
-            Console.WriteLine(chosen);
+            string[] chosen = dict[this.Employee.Name].Value as string[];
+            if (chosen != null)
+            {
+                IEnumerable<Employee> employees = this.Employee.GetPropertiesFromValues(chosen);
+                base.WriteObject(employees, true);
+            }
         }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
