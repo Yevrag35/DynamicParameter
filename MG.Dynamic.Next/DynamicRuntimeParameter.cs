48418a32b090becef327b0752ed06d03c046bfa4
Mike Garvey <mike.garvey@yevrag35.com>
2025-12-06 10:48:57 -0500

    Refactor and standardize codebase formatting
    - Added extensive `.editorconfig` rules for code style, formatting, and naming conventions, including preferences for C# language features.
    - Reformatted `AttributeCollection` and `DynamicRuntimeParameter` classes for consistent indentation and spacing.
    - Simplified methods and improved internal handling of attributes in `AttributeCollection`.
    - Updated `DynamicRuntimeParameter` to ensure non-null `Name` values and renamed `ParameterAttribute` to `DefaultParameter`.
    - Removed unused `using` directives and retained functionality for version checking in `AttributeCollection-Enumeration`.
    - Introduced a placeholder class `ParameterAttributeEquality` for future functionality.
    - Updated `MG.Dynamic.Next.csproj` with additional `Using` directives.
    - Upgraded Visual Studio version in `MG.Dynamic.sln` and added `.editorconfig` to the solution.

---
+++ MG.Dynamic.Next/DynamicRuntimeParameter.cs
@@ -0,0 +1,69 @@
+﻿using MG.Dynamic.Collections;
+using System.Collections.ObjectModel;
+
+namespace MG.Dynamic;
+
+public sealed class DynamicRuntimeParameter : RuntimeDefinedParameter
+{
+	private readonly AttributeCollection _attributes;
+
+	/// <inheritdoc cref="RuntimeDefinedParameter.Name" path="/*[not(self::exception)]"/>
+	public new string Name
+	{
+		get => GetNameField(this);
+		set
+		{
+			if (string.IsNullOrWhiteSpace(value))
+			{
+				value = string.Empty;
+			}
+
+			GetNameField(this) = value;
+		}
+	}
+	/// <summary>
+	/// Gets the default parameter attribute that identifies this parameter to cmdlets.
+	/// </summary>
+	public ParameterAttribute DefaultParameter => _attributes.DefaultParameter;
+
+	/// <inheritdoc cref="RuntimeDefinedParameter.ParameterType" path="/*[not(self::exception)]"/>
+	public new Type? ParameterType
+	{
+		get => GetParameterTypeField(this);
+		set => GetParameterTypeField(this) = value;
+	}
+
+	public DynamicRuntimeParameter(params ReadOnlySpan<Attribute> attributes) : base()
+	{
+		_attributes = AttributeCollection.Create(attributes);
+		GetAttributes(this) = _attributes;
+	}
+	public DynamicRuntimeParameter(string name, Type parameterType, params ReadOnlySpan<Attribute> attributes)
+		: base(name, parameterType, ReturnAndOut(out var collection, attributes))
+	{
+		_attributes = collection;
+	}
+	public DynamicRuntimeParameter(string name, Type parameterType, IEnumerable<Attribute> attributes)
+		: base(name, parameterType, ReturnAndOut(out var collection, attributes))
+	{
+		_attributes = collection;
+	}
+
+	private static AttributeCollection ReturnAndOut(out AttributeCollection collection, ReadOnlySpan<Attribute> values)
+	{
+		return collection = AttributeCollection.Create(values);
+	}
+	private static AttributeCollection ReturnAndOut(out AttributeCollection collection, IEnumerable<Attribute> attributes)
+	{
+		return collection = AttributeCollection.CreateFrom(attributes);
+	}
+
+	[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "<Attributes>k__BackingField")]
+	private static extern ref Collection<Attribute>? GetAttributes(RuntimeDefinedParameter parameter);
+
+	[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_name")]
+	private static extern ref string GetNameField(RuntimeDefinedParameter parameter);
+
+	[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_parameterType")]
+	private static extern ref Type? GetParameterTypeField(RuntimeDefinedParameter parameter);
+}
---
+++ MG.Dynamic.Next/Internal/ParameterAttributeEquality.cs
@@ -0,0 +1,5 @@
+﻿namespace MG.Dynamic.Internal;
+
+internal class ParameterAttributeEquality
+{
+}
